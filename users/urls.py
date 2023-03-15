8d9472abdaf4a1018515560f8044aa236431e997
sheready <csstlw@gmail.com>
2023-03-15 23:11:00 +0300

    add urls for the update user profile, user profile , all users and register user

--- users/urls.py
+++ users/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('register/', views.registerUser, name='register'),
     path('profile/', views.getUserProfile, name='users-profile'),
+    path('profile/update/', views.updateUserProfile, name='user-profile-update'),
     path('all-users/', views.getUsers, name='users'),
 ]
