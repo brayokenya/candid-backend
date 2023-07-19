d6e60b738b3aaa95cfe87ed72e229e807459cae7
sheready <csstlw@gmail.com>
2023-07-19 23:43:49 +0300

    add signal to compress image size

--- users/signals.py
+++ users/signals.py
@@ -1,5 +1,8 @@
 from django.db.models.signals import pre_save
+from django.db.models.signals import post_save
 from django.contrib.auth.models import User
+from PIL import Image
+from users.models import Profile
 
 def updateUser(sender, instance, **kwargs):
     user = instance
@@ -7,3 +10,10 @@ def updateUser(sender, instance, **kwargs):
         user.username = user.email
 
 pre_save.connect(updateUser, sender=User)
+
+def image_compressor(sender, **kwargs):
+    if kwargs["created"]:
+        with Image.open(kwargs["instance"].image_url.path) as photo:
+            photo.save(kwargs['instance'].image_url.path, optimize=True, quality=50)
+
+post_save.connect(image_compressor, sender=Profile)
