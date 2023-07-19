a3bf7095a620887824bc402283c08a5edcdc62ac
sheready <csstlw@gmail.com>
2023-07-19 23:45:37 +0300

    add profile model to admin site

--- users/apps.py
+++ users/apps.py
@@ -4,6 +4,6 @@ from django.apps import AppConfig
 class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'users'
-
+    #configure app to know about our signal
     def ready(self):
         import users.signals
