1756d1384d034daf846607708f87f755ae0d04e0
sheready <csstlw@gmail.com>
2023-07-19 23:43:01 +0300

    edit login url

--- users/urls.py
+++ users/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 
 urlpatterns = [
-    path('login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('login/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('register/', views.registerUser, name='register'),
     path('profile/', views.getUserProfile, name='users-profile'),
     path('profile/update/', views.updateUserProfile, name='user-profile-update'),
