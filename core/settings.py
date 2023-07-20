d907d35b26b4832e20a65e01a6a1a993a679fa9c
sheready <csstlw@gmail.com>
2023-07-20 17:10:06 +0300

    edit media url

--- core/settings.py
+++ core/settings.py
@@ -183,7 +183,7 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Actual directory user files go to
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'mediafiles')
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'candid-backend/media')
 
 # URL used to access the media
-MEDIA_URL = '../media/'
+MEDIA_URL = 'candid-backend/media/'
